1e51cd878b2ce3de50632d5dcce898d09e9aaa68
SK SOHEL RAHAMAN <117460184+sohel-777@users.noreply.github.com>
2023-02-15 11:53:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
-
+function addToList() {
+	//alert("aa");
+	var input = document.querySelector("input").value;
+	if(input){
+		var li=document.createElement("li");
+		li.textContent=input;
+		document.querySelector("ol").appendChild(li);
+		document.querySelector("input").value = "";
+	}
+}
+document.querySelector("button").addEventListener('click', addToList);
